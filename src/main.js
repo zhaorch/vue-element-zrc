b8b7a77de1697e57ef4955e4b77a2ad661311352
zhaorch <zhaorch@163.com>
2019-06-13 16:58:43 +0800

    login

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+import '@/icons' // icon
+
+// set ElementUI lang to EN
+Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
