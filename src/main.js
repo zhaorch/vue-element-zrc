844305a42e305629fc9d183fce91585b4fd774d6
zhaorch <zhaorch@163.com>
2019-06-14 09:05:18 +0800

    login

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
+import '@/styles/index.scss' // global css
+
 import '@/icons' // icon
+
+import App from './App'
+import router from './router'
 
 // set ElementUI lang to EN
 Vue.use(ElementUI, { locale })
